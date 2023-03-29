3c71925d99982788f5007dff48af544c9ba9165e
kevininda <indakevinpc@gmail.com>
2023-03-29 02:03:35 -0300

    inicio

---
+++ views/index.view.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="../css/estilos.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <div class="contenedor">
+            <div class="logoizquierda">
+                <p><a href="#">blog</a></p>
+            </div>
+            <div class="derecha">
+                <form action=""></form>
+            </div>
+        </div>
+    </header>
+</body>
+</html>
