a8d14870e28430d43c3cd37805d912d95bb84048
kevininda <indakevinpc@gmail.com>
2023-04-11 22:14:17 -0300

    login

---
+++ views/header.view.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/estilos.css">
+    <title>Blog</title>
+</head>
+
+<body>
+    <header>
+        <nav class="nav navbar .bg-primary">
+            <div class="container-fluid">
+                <a class="navbar-brand">Logo</a>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Buscar">
+                    <button class="btn btn-success" type="submit">Buscar</button>
+                </form>
+            </div>
+        </nav>
+
+    </header>
--- views/index.view.php
+++ views/index.view.php
@@ -1,25 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/estilos.css">
-    <title>Blog</title>
-</head>
-
-<body>
-    <header>
-        <div class="contenedor">
-            <div class="logoizquierda">
-                <p><a href="#">blog</a></p>
-            </div>
-            <div class="derecha">
-                <input type="text" placeholder="Buscar">
-            </div>
-        </div>
-    </header>
+<?php require 'header.php' ?>
 </body>
+<main role="main">
+    <div class="contenedor">
+        <div class="box">
+            <form action="" method="post">
+                <h1>Acceso</h1>
+                <input type="text" placeholder="Usuario">
+                <input type="password" name="pass" id="pass" placeholder="Contraseña">
+                <div class="btn">
+                    <button type="submit" class="btn btn-success">Ingresar</button>
+                </div>
+            </form>
+
+
+            <!-- <h1><strong>Kevin Inda</strong></h1>
+                <ul>
+                    <li>Edad: 29 años</li>
+                    <li>Ciudad: Argentina</li>
+                    <li>Profesion: Programador de Sistemas</li>
+                    <li>Comida favorita: Asado</li>
+                    <li>Nacionalidad: Argetino</li>
+                </ul>-->
+        </div>
+
+    </div>
+
+</main>
 
 </html>
